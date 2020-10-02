e925ff543ca4d4eaef2200cce015aab002010e25
GUILLERMO2966 <71560170+GUILLERMO2966@users.noreply.github.com>
2020-10-02 16:03:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,70 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . . . .
+            `)
         basic.showLeds(`
             . # # # .
             . # # # .
             # # # # #
-            # . . . #
+            # # # # #
             . # # # .
             `)
     } else if (input.buttonIsPressed(Button.B)) {
         basic.showLeds(`
             . . # . .
-            # # # # .
+            . . # # .
+            . . # # #
+            . . # # .
+            . . # . .
+            `)
+        basic.clearScreen()
+        basic.showLeds(`
+            . . # . .
+            . . # # .
             # # # # #
-            # # # # .
+            . . # # .
             . . # . .
             `)
     } else if (input.buttonIsPressed(Button.A)) {
         basic.showLeds(`
             . . # . .
-            . # # # #
+            . # # . .
+            # # # . .
+            . # # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # . .
             # # # # #
-            . # # # #
+            . # # . .
             . . # . .
             `)
     } else {
         basic.clearScreen()
     }
+})
+basic.forever(function () {
+	
 })
